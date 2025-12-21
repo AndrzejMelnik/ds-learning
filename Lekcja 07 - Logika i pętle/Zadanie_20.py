2c926f4aa5d4883fa0fed7d0dc188c41552a714f
Andrzej M. <andrzej.melnik@interia.eu>
2025-12-21 20:00:19 +0100

    Utworzenie min i max

--- Lekcja 07 - Logika i pętle/Zadanie_20.py
+++ Lekcja 07 - Logika i pętle/Zadanie_20.py
@@ -4,4 +4,9 @@ wartosci = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 #sortowanie listy i z niej utworzenie nowej listy
 new_list = sorted(wartosci)
 
+# wyswietlenie nowej listy; utworzenie min i max
+print(new_list)
+min = new_list[0]
+max = new_list[-1]
+
 
