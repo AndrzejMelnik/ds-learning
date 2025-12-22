163985395eeb95d77730ee8ad5c0ed9903611091
Andrzej M. <andrzej.melnik@interia.eu>
2025-12-22 15:33:19 +0100

    Tworzenie struktury słownika słowników

--- Lekcja 07 - Logika i pętle/Zadanie_19.py
+++ Lekcja 07 - Logika i pętle/Zadanie_19.py
@@ -6,3 +6,13 @@ for value in state_sequence:
     if value not in unique_list:
         unique_list.append(value)
 print(unique_list)
+
+#tworzenie słownika sekwencji
+sequences = {}
+
+# wewnętrzny słownik dla każdego ze stanów
+for starting_state in unique_list:
+    sequences[starting_state] = {}
+    # wszystkie przejścia ustwione z wartością zero
+    for ending_state in unique_list:
+        sequences[starting_state][ending_state] = 0
