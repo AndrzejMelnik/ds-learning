4b2bcf6e3e12054b4b6ca64bb0188ac94b0cab4d
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-14 17:02:44 +0100

    Stworzenie klasy BankAccount wraz z jej konstruktorem.

---
+++ Lekcja 09 - OOP/Zad.3.py
@@ -0,0 +1,14 @@
+""" Zadanie 3 – Klasa BankAccount
+    Stwórz klasę BankAccount z atrybutami: wlasciciel , saldo .
+    Metody:
+    Stwórz kilka kont i przetestuj operacje:
+
+    wplac(kwota) – zwiększa saldo
+    wyplac(kwota) – zmniejsza saldo (tylko jeśli wystarczy środków)
+    pokaz_saldo() – wyświetla saldo """
+
+class BankAccount:
+
+    def __init__(self, wlasciciel, saldo):
+        self.wlasciciel = wlasciciel
+        self.saldo = saldo
