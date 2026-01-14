febb7ed74d868ae615b8f6557ea397c69a3654ff
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-14 18:04:40 +0100

    Dodanie metod: dodaj_ocene() oraz srednia()

--- Lekcja 09 - OOP/Zad.5.py
+++ Lekcja 09 - OOP/Zad.5.py
@@ -13,3 +13,13 @@ class Student:
         self.imie = imie
         self.numer_indeksu = numer_indeksu
         self.oceny = oceny
+
+    def dodaj_ocene(self, ocena):
+        self.oceny.append(ocena)
+
+    def srednia(self):
+        suma = 0
+        for note in self.oceny:
+            suma += note
+        score = round(suma / len(self.oceny),2)
+        return score
