f322b3d9f67bc74cf471af05ec1a8e7fcd9752e7
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-21 19:54:52 +0100

    Napisanie funkcji zwracającej odległosc Euklidesową, zgodnie ze wzorem.

--- Lekcja 10 - Numpy i podstawy pracy z tablicami/Zad.12.py
+++ Lekcja 10 - Numpy i podstawy pracy z tablicami/Zad.12.py
@@ -14,5 +14,7 @@ import numpy as np
 point_a = np.array([1, 2, 3, 4, 5])
 point_b = np.array([5, 4, 3, 2, 1])
 
+def distance_a_b(point_a, point_b):
+    return np.sqrt(np.sum((point_a - point_b) ** 2))
 
-
+print(distance_a_b(point_a, point_b))
