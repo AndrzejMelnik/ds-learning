005f57e31449ef008e2a85408c888f2b9730a93f
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-21 19:48:44 +0100

    Podanie punktów a i b

---
+++ Lekcja 10 - Numpy i podstawy pracy z tablicami/Zad.12.py
@@ -0,0 +1,18 @@
+
+
+""" Część I
+
+Masz 2 punkty w przestrzeni 5-wymiarowej:
+
+point_a = np.array([1, 2, 3, 4, 5])
+point_b = np.array([5, 4, 3, 2, 1])
+
+Oblicz odległość Euklidesową: sqrt(sum((a - b)^2)) """
+
+import numpy as np
+
+point_a = np.array([1, 2, 3, 4, 5])
+point_b = np.array([5, 4, 3, 2, 1])
+
+
+
