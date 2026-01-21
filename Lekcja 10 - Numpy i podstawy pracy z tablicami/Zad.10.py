8603d300d84e4c2d17c255cedcd8623e2706ef12
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-21 19:24:50 +0100

    Wprowadzenie danych (macierzy) exams i bonus.Określenie ich 'kształtu'.

---
+++ Lekcja 10 - Numpy i podstawy pracy z tablicami/Zad.10.py
@@ -0,0 +1,33 @@
+"""Dana tablica ocen (5 studentów x 4 egzaminy)
+
+exams = np.array([
+[78, 85, 92, 88],
+[65, 72, 68, 70],
+[90, 88, 95, 92],
+[55, 60, 58, 62],
+[82, 79, 85, 80]
+])
+
+Dodaj bonus do każdego egzaminu: [5, 10, 5, 10]
+
+Następnie pomnóż wyniki każdego studenta
+przez mnożnik: [1.0, 1.1, 0.95, 1.15, 1.0]"""
+
+import numpy as np
+
+exams = np.array([
+[78, 85, 92, 88],
+[65, 72, 68, 70],
+[90, 88, 95, 92],
+[55, 60, 58, 62],
+[82, 79, 85, 80]
+])
+print("Shape dla 'exams': ", exams.shape)
+
+bonus = np.array([5, 10, 5, 10])
+print("Shape dla 'bonus': ", bonus.shape)
+
+
+
+
+
