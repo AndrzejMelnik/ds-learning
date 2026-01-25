e7c90d8566c3dad8a9c1df392e2b7f8817b56a31
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-25 16:23:27 +0100

    Wczytanie danych

---
+++ Lekcja 12 - Pandas II/Zad.6.py
@@ -0,0 +1,15 @@
+""" Stwórz nową kolumnę Family_Size w Titanic jako sumę SibSp + Parch + 1 (sam
+pasażer).
+Dataset: Titanic
+
+Wymagania:
+Oblicz Family_Size
+Oblicz survival rate według rozmiaru rodziny
+Zidentyfikuj, który rozmiar rodziny miał najwyższy survival rate
+
+Oczekiwany rezultat:
+Tabela z survival rate według Family_Size
+Wykres słupkowy """
+
+import pandas as pd
+titanic_data = pd.read_csv('titanic.csv')
