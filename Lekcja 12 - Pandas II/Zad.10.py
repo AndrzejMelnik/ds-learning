458c993cc99fa6ff7e507aa74257370e70b7fb07
Andrzej M. <andrzej.melnik@interia.eu>
2026-01-25 20:38:43 +0100

    Utworzenie kolumny 'Region', na podstawie długości i szerokości geograficznej.

--- Lekcja 12 - Pandas II/Zad.10.py
+++ Lekcja 12 - Pandas II/Zad.10.py
@@ -12,12 +12,29 @@ Porównaj IQR między regionami
 Oczekiwany rezultat:
 Tabela z IQR dla każdego regionu """
 
+
+from scipy.stats import describe
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import geopip
 
 housing = fetch_california_housing(as_frame=True)
 df_housing = housing.frame
 
 print(df_housing.head())
 
+describe_data = df_housing.describe()
 
+
+
+# iqr = describe_data.loc['75%'] - describe_data.loc['25%']
+# print(iqr)
+
+describe_data['Region'] = (
+    pd.cut(describe_data["Latitude"], bins=3, labels=["Południe",
+          "Centrum", "Północ"]).astype(str) + " " +
+          pd.cut(describe_data["Longitude"],
+          bins=3, labels=["Zachód", "Centrum", "Wschód"]).astype(str)
+)
+# print(describe_data[["Latitude", "Longitude", 'Region']])
+print(describe_data.columns)
