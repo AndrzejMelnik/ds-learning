16304416f26e2f6c639a2110693d90e0544eb246
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-04 19:37:11 +0100

    Wygenerowanie wykresu.

--- Lekcja 16 - Statystyka i prawdopodobieństwo/Zad.9.py
+++ Lekcja 16 - Statystyka i prawdopodobieństwo/Zad.9.py
@@ -12,6 +12,8 @@ Teoretyczny PMF rozkładu B(20, 0.5) nałożony na histogram
 Porównanie średniej empirycznej z teoretyczną"""
 
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import binom
 
 n= 20
 p = 0.5
@@ -19,3 +21,20 @@ size = 1000
 np.random.seed(42)
 coin_flips = np.random.binomial(n , p, size)
 print(coin_flips)
+
+#Wykres
+plt.hist(coin_flips, bins=np.arange(-0.5, n + 1.5, 1),
+         density=True, edgecolor='black')
+
+x = np.arange(0, n+1)
+pmf = binom.pmf(x, n, p)
+
+plt.scatter(x, pmf, color='red', label='Teoretyczny PMF')
+plt.vlines(x, 0, pmf, colors='red', linestyles='dashed')
+
+plt.title("Histogram z rozkładem dwumianowym (n=20, p=0.5)")
+plt.xlabel("Liczba wyrzuconych orłów")
+plt.ylabel("Stopień prawdopodobieństwa")
+plt.legend()
+plt.grid(True)
+plt.show()
