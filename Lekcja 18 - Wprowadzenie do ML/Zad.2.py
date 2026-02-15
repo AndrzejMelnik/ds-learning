c4fd0d4230e602a7d0286f372aa8496225b2d91f
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-15 17:37:35 +0100

    Wyświetlenie rozmiarów zbioru treningowego i testowego.

--- Lekcja 18 - Wprowadzenie do ML/Zad.2.py
+++ Lekcja 18 - Wprowadzenie do ML/Zad.2.py
@@ -16,3 +16,6 @@ X, y = wine.data, wine.target
 
 X_train, X_test, y_train, y_test = train_test_split(
 X, y, test_size=0.3, random_state=42,stratify=y)
+
+print(f"Rozmiar zbioru treningowego: {X_train.shape[0]}")
+print(f"Rozmiar zbioru testowego: {X_test.shape[0]}")
