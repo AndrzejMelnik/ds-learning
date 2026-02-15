df1e4105899ade6d0eba41e8f400e6d96a73ce7a
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-15 17:34:38 +0100

    Podział na zbiór testowy i treningowy; import train_test_split.

--- Lekcja 18 - Wprowadzenie do ML/Zad.2.py
+++ Lekcja 18 - Wprowadzenie do ML/Zad.2.py
@@ -9,7 +9,10 @@ Ten sam podział train/test dla wszystkich
 Wydrukuj wyniki w formie tabeli"""
 
 import sklearn
-
+from sklearn.model_selection import train_test_split
 
 wine = sklearn.datasets.load_wine()
 X, y = wine.data, wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+X, y, test_size=0.3, random_state=42,stratify=y)
