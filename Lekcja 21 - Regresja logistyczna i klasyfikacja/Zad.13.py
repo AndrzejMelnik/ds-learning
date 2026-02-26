3cd29b307d783577ea93bc498c7c63287f0dba83
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-26 17:05:38 +0100

    Standaryzacja.

--- Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.13.py
+++ Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.13.py
@@ -13,6 +13,7 @@ Oczekiwany rezultat
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 X, y = make_classification(
 n_samples=1000,
@@ -27,3 +28,8 @@ random_state=42
 X_train, X_test, y_train, y_test = train_test_split(
 X, y, test_size=0.2, random_state=42, stratify=y
 )
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+results = {}
