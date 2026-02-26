86a23604a9ef7488ad8aa9684f0a224b719e77a1
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-26 17:04:58 +0100

    Podział danych.

--- Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.13.py
+++ Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.13.py
@@ -12,6 +12,7 @@ Oczekiwany rezultat
     Analiza trade-offu"""
 
 from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 
 X, y = make_classification(
 n_samples=1000,
@@ -21,4 +22,8 @@ n_redundant=5,
 n_classes=2,
 weights=[0.95, 0.05],
 random_state=42
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+X, y, test_size=0.2, random_state=42, stratify=y
 )
