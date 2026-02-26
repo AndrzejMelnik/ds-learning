66aa0c585182de464d1360fec7deadc4aac851a8
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-26 16:53:48 +0100

    Wizualizacja danych.

--- Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.18.py
+++ Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.18.py
@@ -11,6 +11,7 @@ Oczekiwany rezultat:
     Wykres learning curves
     Analiza overfitting/underfitting"""
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import learning_curve
@@ -31,3 +32,25 @@ train_sizes, train_scores, val_scores = learning_curve(
     scoring='accuracy',
     n_jobs=-1
 )
+
+#Wizualizacja danych
+plt.plot(train_sizes, train_scores.mean(axis=1), 'o-',
+         label='Train Score', color='red')
+plt.plot(train_sizes, val_scores.mean(axis=1), 'o-',
+         label='Validation Score', color='green')
+
+plt.fill_between(train_sizes,
+                 train_scores.mean(axis=1) - train_scores.std(axis=1),
+                 train_scores.mean(axis=1) + train_scores.std(axis=1),
+                 alpha=0.1, color='red')
+
+plt.fill_between(train_sizes,
+                 val_scores.mean(axis=1) - val_scores.std(axis=1),
+                 val_scores.mean(axis=1) + val_scores.std(axis=1),
+                 alpha=0.1, color='green')
+plt.xlabel('Rozmiar zbioru treningowego')
+plt.ylabel('Accuracy')
+plt.title('Learning Curves')
+plt.legend(loc='best')
+plt.grid(True, alpha=0.3)
+plt.show()
