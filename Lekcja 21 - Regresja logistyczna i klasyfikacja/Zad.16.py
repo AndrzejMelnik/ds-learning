d87916fba5e1af49ce8d9d621fae215f1bdb635c
Andrzej M. <andrzej.melnik@interia.eu>
2026-02-26 16:05:48 +0100

    Wczytanie danych

---
+++ Lekcja 21 - Regresja logistyczna i klasyfikacja/Zad.16.py
@@ -0,0 +1,17 @@
+"""Zadanie 16 – Cross-validation dla klasyfikacji
+    Przeprowadź walidację krzyżową modelu klasyfikacyjnego.
+Dataset:
+    Breast Cancer dataset
+Wymagania:
+    Użyj StratifiedKFold (5 foldów)
+    Oblicz średnią i odchylenie standardowe dla: accuracy, precision, recall, f1
+    Użyj cross_val_score i cross_validate
+Oczekiwany rezultat:
+    Tabela z metrykami (mean ± std)
+    Boxplot metryk z CV"""
+
+
+from sklearn.datasets import load_breast_cancer
+
+data = load_breast_cancer()
+X, y = data.data, data.target
