02078d5e2990d2d621a0a2b866dfe5ed6816f150
Andrzej M. <andrzej.melnik@interia.eu>
2026-03-02 17:12:14 +0100

    Standaryzacja.

--- Lekcja 22 - Regularyzacja i kontrola dopasowania/Zad.7.py
+++ Lekcja 22 - Regularyzacja i kontrola dopasowania/Zad.7.py
@@ -9,6 +9,7 @@ Ewaluacja na oddzielnym zbiorze testowym"""
 
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 california = fetch_california_housing()
 X = california.data
@@ -16,3 +17,7 @@ y = california.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
 random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
