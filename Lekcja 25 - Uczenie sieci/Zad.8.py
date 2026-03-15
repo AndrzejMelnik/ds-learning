e0acb8f82514457f962b55b5f37621deea2aac2a
Andrzej M. <andrzej.melnik@interia.eu>
2026-03-15 15:30:44 +0100

    Podział danych na train i test.

--- Lekcja 25 - Uczenie sieci/Zad.8.py
+++ Lekcja 25 - Uczenie sieci/Zad.8.py
@@ -11,6 +11,11 @@ Porownaj: epoki, czas, accuracy
 Tabela porownawcza i wykresy loss"""
 
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 
 cancer_data = load_breast_cancer()
 X, y = cancer_data.data, cancer_data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
