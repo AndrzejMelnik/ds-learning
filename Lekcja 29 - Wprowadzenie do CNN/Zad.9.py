350befd97221ff03822910a4fc6b24b01faeaf24
Andrzej M. <andrzej.melnik@interia.eu>
2026-03-30 15:16:33 +0200

    Normalizacja.

--- Lekcja 29 - Wprowadzenie do CNN/Zad.9.py
+++ Lekcja 29 - Wprowadzenie do CNN/Zad.9.py
@@ -8,10 +8,15 @@ EarlyStopping + ReduceLROnPlateau
 Krzywe uczenia (loss, accuracy)
 Confusion matrix z nazwami klas
 Analiza bledow"""
-
+import numpy as np
 from tqdm import keras
 
 class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+x_train = x_train[..., np.newaxis]
+x_test = x_test[..., np.newaxis]
