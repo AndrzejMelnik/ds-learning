afd93791281466966cdb4e2f1f84d312c931cf59
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-02 17:20:07 +0200

    Budowa obu modeli.

--- Lekcja 30 - Warstwy i klasyfikacje obrazów/Zad.3.py
+++ Lekcja 30 - Warstwy i klasyfikacje obrazów/Zad.3.py
@@ -17,3 +17,21 @@ def build_model(conv_type='standard'):
         conv_layer = layers.Conv2D
     else:
         conv_layer = layers.SeparableConv2D
+
+
+    model.add(conv_layer(32, (3, 3), padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+
+    model.add(conv_layer(64, (3, 3), padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(conv_layer(128, (3, 3), padding='same', activation='relu'))
+    model.add(layers.GlobalAveragePooling2D())  # Redukcja parametrów klasyfikatora
+
+    model.add(layers.Dense(10, activation='softmax'))
+    return model
+
+model_std = build_model(conv_type='standard')
+model_sep = build_model(conv_type='separable')
+
