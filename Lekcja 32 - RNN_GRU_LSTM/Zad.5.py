3a5c582eaa0dbabc7fd1ad0915d7c67c1ae995da
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-17 12:48:42 +0200

    Pętla eksperymentalna.

--- Lekcja 32 - RNN_GRU_LSTM/Zad.5.py
+++ Lekcja 32 - RNN_GRU_LSTM/Zad.5.py
@@ -9,9 +9,22 @@ Tabela: maxlen, accuracy, czas treningu
 Interpretacja"""
 
 from tensorflow import keras
+from tensorflow.keras import layers
 
 max_features = 10000
 maxlen_values = [2-5]
 results = []
 
 (x_train_raw, y_train),(x_test_raw, y_test) = keras.datasets.imdb.load_data(num_words=max_features)
+
+for ml in maxlen_values:
+    print(f"\n--- Testowanie maxlen = {ml} ---")
+    x_train = keras.preprocessing.sequence.pad_sequences(x_train_raw, maxlen=ml)
+    x_test = keras.preprocessing.sequence.pad_sequences(x_test_raw, maxlen=ml)
+    model = keras.Sequential([
+        layers.Embedding(max_features, 64),
+        layers.LSTM(64, dropout=0.2),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
