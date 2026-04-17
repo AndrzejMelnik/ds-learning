9eca4ba61927376579d950d42cf175cc964a4810
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-17 12:35:38 +0200

    Przygotowanie danych.

---
+++ Lekcja 32 - RNN_GRU_LSTM/Zad.3.py
@@ -0,0 +1,8 @@
+from tensorflow import keras
+
+max_features = 5000
+maxlen = 100
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words=max_features)
+x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
