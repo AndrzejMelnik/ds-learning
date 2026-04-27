7dd31e95ba6ec90c822354c713fb06775b6316d5
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-27 16:18:08 +0200

    Przygotowanie korpusu (20 zdań) oraz trening modelu Word2Vec.

---
+++ Lekcja 34 - Podstawy NLP i reprezentacje/Zad.5.py
@@ -0,0 +1,30 @@
+"""Zadanie 5 -- Wizualizacja embeddingów
+Wytrenuj model Word2Vec na prostym korpusie (np. 20 zdan o zwierzetach, kolorach,
+rozmiarach). Zwizualizuj embeddingi za pomoca PCA (2D). Sprawdz czy semantycznie
+podobne slowa sa blisko siebie.
+Oczekiwany wynik: wykres PCA z pogrupowanymi slowami"""
+
+from gensim.models import Word2Vec
+
+sentences = [
+    ["mały", "biały", "pies", "biega"], ["duży", "czarny", "pies", "szczeka"],
+    ["mały", "biały", "kot", "śpi"], ["duży", "czarny", "kot", "mruczy"],
+    ["czerwony", "duży", "ptak", "lata"], ["niebieski", "mały", "ptak", "śpiewa"],
+    ["zielony", "mały", "żaba", "skacze"], ["żółty", "duży", "lew", "ryczy"],
+    ["mały", "biały", "chomik", "je"], ["duży", "szary", "słoń", "trąbi"],
+    ["mały", "czarny", "pająk", "tka"], ["duży", "brązowy", "niedźwiedź", "ryczy"],
+    ["niebieski", "duży", "wieloryb", "płynie"], ["mały", "żółty", "kanarek", "śpiewa"],
+    ["zielony", "duży", "krokodyl", "pływa"], ["biały", "duży", "koń", "galopuje"],
+    ["czarny", "mały", "kret", "kopie"], ["szary", "mały", "wilk", "wyje"],
+    ["brązowy", "duży", "jeleń", "biegnie"], ["czerwony", "mały", "lis", "poluje"]
+]
+
+model = Word2Vec(
+    sentences,
+    vector_size=50,  
+    window=3,
+    min_count=1,
+    sg=1,
+    epochs=100,
+    seed=42
+)
