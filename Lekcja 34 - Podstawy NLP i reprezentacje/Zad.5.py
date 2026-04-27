57696f75d0ed9f302dbe3b9ad828c6c45f670457
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-27 16:18:54 +0200

    Przygotowanie danych do wizualizacji.

--- Lekcja 34 - Podstawy NLP i reprezentacje/Zad.5.py
+++ Lekcja 34 - Podstawy NLP i reprezentacje/Zad.5.py
@@ -5,6 +5,7 @@ podobne slowa sa blisko siebie.
 Oczekiwany wynik: wykres PCA z pogrupowanymi slowami"""
 
 from gensim.models import Word2Vec
+import numpy as np
 
 sentences = [
     ["mały", "biały", "pies", "biega"], ["duży", "czarny", "pies", "szczeka"],
@@ -21,10 +22,13 @@ sentences = [
 
 model = Word2Vec(
     sentences,
-    vector_size=50,  
+    vector_size=50,
     window=3,
     min_count=1,
     sg=1,
     epochs=100,
     seed=42
 )
+
+words = list(model.wv.index_to_key)
+word_vectors = np.array([model.wv[w] for w in words])
