458187349995f456b1d6c29a423df004ca12687d
Andrzej M. <andrzej.melnik@interia.eu>
2026-04-27 17:07:11 +0200

    Funkcja budująca model LSTM.

--- Lekcja 35 - Praktyka NLP/Zad.10.py
+++ Lekcja 35 - Praktyka NLP/Zad.10.py
@@ -9,22 +9,29 @@ Pretrenowanymi GloVe (odblokowanymi, trainable=True)"""
 
 from tensorflow import keras
 from tensorflow.keras import layers
-max_features = 10000
-maxlen = 200
+import numpy as np
+
+max_features = 10000  # Rozmiar słownika [3]
+maxlen = 200          # Dobry kompromis dla recenzji IMDB [4]
+embed_dim = 100       # Standardowy wymiar dla GloVe [5]
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words=max_features)
 x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
 
-def build_model(embedding_layer, name):
+def get_simulated_glove_weights(vocab_size, dim):
+    return np.random.randn(vocab_size, dim) * 0.01
+
+embedding_matrix = get_simulated_glove_weights(max_features, embed_dim)
+
+
+def build_lstm_model(embedding_layer, name):
     model = keras.Sequential([
         layers.Input(shape=(maxlen,)),
         embedding_layer,
-        layers.LSTM(64, dropout=0.3, recurrent_dropout=0.2), # Parametry stabilizujące [3]
+        layers.LSTM(64, dropout=0.2, recurrent_dropout=0.2),
         layers.Dense(1, activation='sigmoid')
     ], name=name)
+
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
-
-emb_scratch = layers.Embedding(max_features, 100, trainable=True)
-model_scratch = build_model(emb_scratch, "LSTM_Scratch")
