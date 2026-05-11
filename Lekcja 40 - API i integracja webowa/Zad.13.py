2e4f1cf08de7abda915ba46e11b43caa2d4f7228
Andrzej M. <andrzej.melnik@interia.eu>
2026-05-11 15:55:47 +0200

    Trenowanie modeli.

--- Lekcja 40 - API i integracja webowa/Zad.13.py
+++ Lekcja 40 - API i integracja webowa/Zad.13.py
@@ -5,6 +5,14 @@ Klient wybiera wersję modelu.
 Oczekiwany wynik: API z 3 modelami, porównanie predykcji
 """
 from sklearn.datasets import load_breast_cancer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
 
 data = load_breast_cancer()
 X, y = data.data, data.target
+
+model_v1 = make_pipeline(StandardScaler(), LogisticRegression()).fit(X, y)
+model_v2 = RandomForestClassifier(n_estimators=100, max_depth=10).fit(X, y)
+model_v3 = GradientBoostingClassifier(n_estimators=100).fit(X, y)
